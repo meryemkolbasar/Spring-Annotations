50447c2f5f8b77f1582135e39b66ec96669d0282
Meryem Kolbasar <122554312+meryemkolbasar@users.noreply.github.com>
2023-06-10 05:09:30 +0200

    Update AppConfiguration.java

--- src/main/java/com/tpe/AppConfiguration.java
+++ src/main/java/com/tpe/AppConfiguration.java
@@ -10,33 +10,25 @@ import org.springframework.core.env.Environment;
 import java.util.Properties;
 import java.util.Random;
 
-@Configuration//bu classta config yapılacak
-@ComponentScan("com.tpe")//bu packagedaki tüm componentları tara,default:com.tpe
+@Configuration
+@ComponentScan("com.tpe")
 @PropertySource("classpath:application.properties")
 public class AppConfiguration {
 
     @Autowired
-    private Environment environment;// Environment(Springin interfacei) application.properties
-    // içindeki keylerin değerini okuyabilmemizi sağlar
+    private Environment environment;
 
-
-    @Bean//random classından Spring bean oluşmasını sağlar.
+    @Bean
     public Random random(){
         return new Random();
     }
 
-
-    //value annotation ile yaptığımızı properties classı ile de yapabiliriz.
-
     @Bean
     public Properties properties(){
-        Properties properties=new Properties();//hashtable extend eder
+        Properties properties=new Properties();
         properties.put("mymail",environment.getProperty("app.email"));
-        //properties.put("mymail","app@gmail.com");
+      
         return properties;
     }
-
-
-
 
 }
